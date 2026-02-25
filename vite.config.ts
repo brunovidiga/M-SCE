e9446a3218a78c09301576d4e4f82bb071376b1b
brunovidiga <brunovidal2804@gmail.com>
2026-02-25 19:08:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(( ) => ({
-  base: "/M-SCE/", // Isso avisa ao projeto que ele está na pasta M-SCE do GitHub
+export default defineConfig(() => ({
   server: {
     host: "::",
     port: 8080,
